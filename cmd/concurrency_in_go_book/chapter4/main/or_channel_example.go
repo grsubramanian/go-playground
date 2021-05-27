2cc0ab4f625762435b59c179ba521aec7421c4bf
Gokul Ramanan Subramanian <gokusubr@amazon.com>
2021-05-27 17:57:23 +0100

    Implement chapter 4 or channel

---
+++ cmd/concurrency_in_go_book/chapter4/main/or_channel_example.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gokul2411s/concurrency/pkg/concurrency_in_go_book/chapter4"
+	"time"
+)
+
+func main() {
+	timeout := func(after time.Duration) <-chan interface{} {
+		out := make(chan interface{})
+		go func() {
+			defer close(out)
+			time.Sleep(after)
+		}()
+		return out
+	}
+
+	startTime := time.Now()
+	quickestTimeout := chapter4.Or(
+		timeout(1*time.Second),
+		timeout(1*time.Minute),
+		timeout(1*time.Hour))
+
+	<-quickestTimeout
+	fmt.Printf("Time for completion: %v\n", time.Since(startTime))
+}
---
+++ pkg/concurrency_in_go_book/chapter4/or_channel.go
@@ -0,0 +1,39 @@
+package chapter4
+
+/**
+ * Closes when any of the underlying channels close or get written to.
+ */
+var Or func(channels ...<-chan interface{}) <-chan interface{}
+
+func init() {
+	Or = func(channels ...<-chan interface{}) <-chan interface{} {
+
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+
+		orDone := make(chan interface{})
+		go func() {
+			defer close(orDone)
+
+			switch len(channels) {
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-Or(append(channels[3:], orDone)...):
+				}
+			}
+		}()
+		return orDone
+	}
+}
